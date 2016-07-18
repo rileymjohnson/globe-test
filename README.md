274c0d95f56df7c227cb661405688a83a82550ef
Riley Johnson7 <rjohnson7@Lenovo.com>
2016-07-18 09:23:56 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # globe-test
-d3 globe written in d3. Allowed you to create tours based on simple json data.
+3d globe written in d3. Allowed you to create tours based on simple json data.
 
 This cannot be run just by opening the index.html file because it requests a local json file which is a CORs violation. To run it you need to serve it. There are a million ways to do this. Easiest is to navigate to the directory and run a python file server.
 
--- index.js
+++ index.js
@@ -17,94 +17,94 @@ create canvas and set up default values
 var globeCanvas = d3.select("body").append("canvas") //canvas dom element to hold globe
 globeCanvas.attr("width", width)
 globeCanvas.attr("height", height)
-    //globeCanvas.style("background-color", "purple")
-    //any additional attributes here
+	//globeCanvas.style("background-color", "purple")
+	//any additional attributes here
 
 var c = globeCanvas.node().getContext("2d"); //2d rendering context to draw continents on
 
 var path = d3.geo.path()
-    .projection(projection)
-    .context(c);
+	.projection(projection)
+	.context(c);
 
 /*
 asynchronously load country data files
 when loaded called loaded ready function
 */
 queue()
-    .defer(d3.json, "d3.json")
-    .await(ready);
+	.defer(d3.json, "d3.json")
+	.await(ready);
 
 function ready(error, world) { //called when data files are loaded
-    if (error) { //will change this
-        alert("the data was not loaded properly")
-    }
+	if (error) { //will change this
+		alert("the data was not loaded properly")
+	}
 
-    var globe = {
-        type: "Sphere"
-    };
-    var land = topojson.feature(world, world.objects.land);
-    var countries = topojson.feature(world, world.objects.countries).features;
-    var borders = topojson.mesh(world, world.objects.countries, function(a, b) {
-        return a !== b;
-    });
+	var globe = {
+		type: "Sphere"
+	};
+	var land = topojson.feature(world, world.objects.land);
+	var countries = topojson.feature(world, world.objects.countries).features;
+	var borders = topojson.mesh(world, world.objects.countries, function(a, b) {
+		return a !== b;
+	});
 
-    countries = [ //sample country data
-        {
-            name: "new york",
-            coordinates: {
-                lat: 40.7128,
-                lng: 74.0059
-            }
-        }, {
-            name: "san fransisco",
-            coordinates: {
-                lat: 37.7749,
-                lng: 122.4194
-            }
-        }, {
-            name: "london",
-            coordinates: {
-                lat: 51.5074,
-                lng: 0.1278
-            }
-        }, {
-            name: "sao paulo",
-            coordinates: {
-                lat: -23.5505,
-                lng: 46.6333
-            }
-        }
-    ];
+	countries = [ //sample country data
+		{
+			name: "new york",
+			coordinates: {
+				lat: 40.7128,
+				lng: 74.0059
+			}
+		}, {
+			name: "san fransisco",
+			coordinates: {
+				lat: 37.7749,
+				lng: 122.4194
+			}
+		}, {
+			name: "london",
+			coordinates: {
+				lat: 51.5074,
+				lng: 0.1278
+			}
+		}, {
+			name: "sao paulo",
+			coordinates: {
+				lat: -23.5505,
+				lng: 46.6333
+			}
+		}
+	];
 
-    var i = -1; //variable for looping through country objects
-    var n = countries.length;
+	var i = -1; //variable for looping through country objects
+	var n = countries.length;
 
-    (function transition() {
-        d3.transition()
-            .duration(1250) //ms it takes for globe to change locations
-            .each("start", function() {
-                i = (i + 1) % n; //adds one. mod n restarts the number when it gets to the end of the list
+	(function transition() {
+		d3.transition()
+			.duration(1250) //ms it takes for globe to change locations
+			.each("start", function() {
+				i = (i + 1) % n; //adds one. mod n restarts the number when it gets to the end of the list
 
-                //this is the spot where you would update all of the other charts and graphs and stuff. Right now I'm just logging the name
-                console.log(countries[i].name)
-            })
-            .tween("rotate", function() {
-                //needs to be in form: north, west
-                lat = countries[i].coordinates.lat;
-                lng = countries[i].coordinates.lng;
-                var r = d3.interpolate(projection.rotate(), [lng, -lat]);
-                return function(t) {
-                    projection.rotate(r(t)); //rotates the map
-                    c.clearRect(0, 0, width, height); //resets the position
-                    c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill(); //land color
-                    var center = projection([-lng, lat]);
-                    c.strokeStyle = "#000", c.fillStyle = "#f00", c.beginPath(), c.arc(center[0], center[1], 5, 0, 2 * Math.PI, false), c.lineWidth = 2, c.fill(), c.stroke(); //draws on point
-                    c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke(); //border colors
-                    c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke(); //globe border color
-                };
-            })
-            .transition()
-            .delay(2000) //ms globe waits at each location
-            .each("end", transition);
-    })();
+				//this is the spot where you would update all of the other charts and graphs and stuff. Right now I'm just logging the name
+				console.log(countries[i].name)
+			})
+			.tween("rotate", function() {
+				//needs to be in form: north, west
+				lat = countries[i].coordinates.lat;
+				lng = countries[i].coordinates.lng;
+				var r = d3.interpolate(projection.rotate(), [lng, -lat]);
+				return function(t) {
+					projection.rotate(r(t)); //rotates the map
+					c.clearRect(0, 0, width, height); //resets the position
+					c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill(); //land color
+					var center = projection([-lng, lat]);
+					c.strokeStyle = "#000", c.fillStyle = "#f00", c.beginPath(), c.arc(center[0], center[1], 5, 0, 2 * Math.PI, false), c.lineWidth = 2, c.fill(), c.stroke(); //draws on point
+					c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke(); //border colors
+					c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke(); //globe border color
+				};
+			})
+			.transition()
+			.delay(2000) //ms globe waits at each location
+			.each("end", transition);
+	})();
 }
